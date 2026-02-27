0d7ec1b5c2182ca33edfdbab3545b784b06362ce
Ghostdevol <karmastudiosbestz@gmail.com>
2026-02-26 22:02:27 -0600

    Initial commit: CraneSlotApp with Capacitor setup

---
+++ android/app/src/main/java/com/ghostech/designs/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ghostech.designs;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/app.js
@@ -0,0 +1,4 @@
+document.getElementById("app").innerHTML = `
+  <h1>Crane Slot App</h1>
+  <p>Your slot and claw game will appear here.</p>
+`;
